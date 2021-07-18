233d359e986c3815499c598fcd244cccdf8c9a74
Naveen Ambookken <naveenambookken@gmail.com>
2021-07-18 18:47:29 +0530

    initial commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid, Paper, Button, Typography, Link, Divider } from "@material-ui/core";
 import Avatar from "@material-ui/core/Avatar";
+import Icon from '@material-ui/core/Icon';
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
@@ -23,7 +24,7 @@ function Login() {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
   const paperStyle = {
-    height: 450,
+    
     width: !matches ? 300 : 600,
     margin: "50px auto",
   };
@@ -71,8 +72,8 @@ function Login() {
           <Grid
             container
             direction="row"
-            justifyContent="center"
-            alignItems="center"
+            
+            
           >
             <Grid item xs={12} sm={6} style={{ padding: 20 }}>
               <Grid align="center">
@@ -130,8 +131,20 @@ function Login() {
                   </Form>
                 )}
               </Formik>
-              <Grid align="center">
-              Connect with
+              
+              <Grid align="center" style={{marginBottom:"20px"}} >
+              <Box style={{margin:"10px"}}>OR</Box>
+              <Box style={{display:"flex", justifyContent:"center", alignItems:"center" ,marginBottom:"10px" ,textAlign: 'center'}}>Connect with
+              
+              <Icon style={{marginLeft:"5px"}}>
+              <img style={{height:"100%"}} src="https://img.icons8.com/color/24/000000/google-logo.png"/>
+              </Icon>
+            
+              <Icon style={{marginLeft:"05px"}} >
+              <img style={{height:"100%"}} src="https://img.icons8.com/color/24/000000/facebook-new.png" alt=""/>
+                
+              </Icon>
+              </Box>
                  
               </Grid>
               <Typography>
@@ -149,8 +162,7 @@ function Login() {
             </Grid>
             <Grid item xs={6} style={{ display: !matches ? "none" : "block" }}>
               <img
-                width="300"
-                height="450"
+                style={{width:"100%", height:"100%", objectFit:"cover"}}
                 src="https://images.all-free-download.com/images/graphiclarge/digital_technology_background_modern_silhouette_3d_design_6837527.jpg"
                 alt="i"
               />
